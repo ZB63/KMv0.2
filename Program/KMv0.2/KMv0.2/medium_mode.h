d220ac6723e000f20a9db152bc5d6df1d614ccfb
ZB63 <spaweloszek63@gmail.com>
2019-02-27 14:26:11 +0100

    dodany hard_mode, drobne zmiany

--- Program/KMv0.2/KMv0.2/medium_mode.h
+++ Program/KMv0.2/KMv0.2/medium_mode.h
@@ -3,24 +3,10 @@
 #include "stdafx.h"
 using namespace std;
 
-void delayy()//nie korzystam z tego
-{
-
-	for (int i = 0; i < 5000000; i++)
-	{
-		for (int i = 0; i < 80; i++)
-		{
-
-		}
-	}
-}
-
-
 void medium_mode(sf::RenderWindow& okno, int x, int y)
 {
 
 	sf::Event zdarzenie;
-	srand(time(NULL));
 
 	Box words;
 	words.fill_box("words");
@@ -36,18 +22,25 @@ void medium_mode(sf::RenderWindow& okno, int x, int y)
 	string random = "";
 	string typed = "";
 	char key = 0;
+	int lives = 3;
 
 	sf::Text tekst_random(random, arial);
-	tekst_random.setCharacterSize(50);
+	tekst_random.setCharacterSize(70);
 	tekst_random.setColor(sf::Color::Yellow);
 	tekst_random.setStyle(sf::Text::Bold);
-	tekst_random.setPosition(x / 3, y / 20 * 3);
+	tekst_random.setPosition(x / 4, y / 20 * 6);
 
 	sf::Text tekst_typed(typed, arial);
-	tekst_typed.setCharacterSize(50);
+	tekst_typed.setCharacterSize(70);
 	tekst_typed.setColor(sf::Color::Yellow);
 	tekst_typed.setStyle(sf::Text::Bold);
-	tekst_typed.setPosition(x / 3, y / 20 * 5);
+	tekst_typed.setPosition(x / 4, y / 20 * 10);
+
+	sf::Text tekst_lives("", arial);
+	tekst_lives.setCharacterSize(50);
+	tekst_lives.setColor(sf::Color::Yellow);
+	tekst_lives.setStyle(sf::Text::Bold);
+	tekst_lives.setPosition(x / 4 * 3, y / 20);
 
 	while (okno.isOpen())//petla glowna
 	{
@@ -56,6 +49,7 @@ void medium_mode(sf::RenderWindow& okno, int x, int y)
 		tekst_random.setString(random);
 		typed = "";
 		tekst_typed.setString(typed);
+		lives = 3;
 
 		for (int i = 0; i < random.length(); i++)//iteracja na kazda litere
 		{
@@ -82,18 +76,26 @@ void medium_mode(sf::RenderWindow& okno, int x, int y)
 						}
 						else {
 							letters.play("false");
+							lives--;
+							if (lives == 0) break;
 						}
 					}
 
 				}
+				if(lives==3) tekst_lives.setString("");
+				else if(lives==2) tekst_lives.setString("X");
+				else if (lives == 1) tekst_lives.setString("XX");
+				else if (lives == 0) tekst_lives.setString("XXX");
 
 				tekst_typed.setString(typed);
 
 				okno.clear();
 				okno.draw(tekst_random);
 				okno.draw(tekst_typed);
+				okno.draw(tekst_lives);
 				okno.display();//wyswietalnie tekstu
 			}
+			if (lives == 0) break;
 		}
 	}
 
